896c25c99d899ba07964f2ad4693f35493465823
unknown <osdroix159@gmail.com>
2025-12-10 11:26:14 -0600

    update of the nomina of create tables

--- Nomina.sql
+++ Nomina.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Nomina (
     id_nomina INT IDENTITY(1,1) NOT NULL,
-    id_empresa INT,
-    id_sucursal INT,
+    id_empresa INT NOT NULL,
+    id_sucursal INT NOT NULL,
     id_empleado INT NOT NULL,
     id_detalle_pago INT NOT NULL,
     id_periodo INT,
@@ -12,4 +12,61 @@ CREATE TABLE Nomina (
     
     -- Definición de la Llave Primaria
     CONSTRAINT PK_Nomina PRIMARY KEY (id_nomina)
+);
+
+CREATE TABLE Detalledenomina (
+    id_detallenomina INT IDENTITY(1,1) NOT NULL,
+    id_nomina INT NOT NULL, -- FK: Viene de la tabla Nomina
+    nom_pago VARCHAR(50) NOT NULL, 
+    id_empresa INT NOT NULL,
+    id_sucursal INT NOT NULL,
+    id_periodo INT NOT NULL,         -- FK: Viene de la tabla Periodo
+    sub_monto DECIMAL(10,2),
+    clave_usuario_u VARCHAR(50) NOT NULL,
+    Create_time DATETIME,
+    Create_date DATETIME,
+
+    -- Definición de la Llave Primaria
+    CONSTRAINT PK_DetalleNomina PRIMARY KEY (id_detallenomina)
+);
+
+CREATE TABLE Periodo (
+    id_periodo INT IDENTITY(1,1) NOT NULL,
+    descripcion VARCHAR(50), -- Para 'semanal', 'quincenal', etc.
+    clave_usuario_u VARCHAR(50) NOT NULL,
+    Create_time DATETIME,
+    Create_date DATETIME,
+
+    -- Definición de la Llave Primaria
+    CONSTRAINT PK_Periodo PRIMARY KEY (id_periodo)
+);
+
+CREATE TABLE catalogo_pagos (
+    id_tipo_pago INT IDENTITY(1,1) NOT NULL,
+    id_empresa INT NOT NULL,
+    id_sucursal INT NOT NULL,
+    nombre_pago VARCHAR(150) NOT NULL,
+    descripcion VARCHAR(150) NOT NULL,
+    monto_base DECIMAL(10,2),
+    activo BIT, -- 1 para activo, 0 para inactivo
+    clave_usuario_u VARCHAR(50) NOT NULL,
+    Create_time DATETIME,
+    Create_date DATETIME,
+
+    CONSTRAINT PK_CatalogoPagos PRIMARY KEY (id_tipo_pago)
+);
+
+CREATE TABLE cuentasBancarias (
+    id_bancaria INT IDENTITY(1,1) NOT NULL,
+    id_empleado INT NOT NULL, 
+    id_empresa INT NOT NULL,
+    id_sucursal INT NOT NULL,
+    clave_bancaria VARCHAR(20) NOT NULL, 
+    nom_banco VARCHAR(150) NOT NULL,
+    clave_usuario_u VARCHAR(50) NOT NULL,
+    Create_time DATETIME,
+    Create_date DATETIME,
+
+    -- Definición de la Llave Primaria
+    CONSTRAINT PK_CuentasBancarias PRIMARY KEY (id_bancaria)
 );
