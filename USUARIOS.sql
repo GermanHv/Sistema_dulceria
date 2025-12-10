84f2d5ff5fa669461b4e9640d25729e3d44b2737
Germán <ghove7@gmail.com>
2025-12-10 16:49:55 -0600

    prueba

--- USUARIOS.sql
+++ USUARIOS.sql
@@ -8,23 +8,19 @@ CREATE TABLE Usuarios (
     id_empresa INT NOT NULL,                            -- FK
     id_sucursal INT,                           -- Puede ser NULL si es admin global
     
-    -- Datos personales
-    nom VARCHAR(150) NOT NULL,
-    app VARCHAR(150) NOT NULL,
-    rfc VARCHAR(13) NOT NULL,               
-    telefono VARCHAR(10) NOT NULL,
-    correo VARCHAR(40) NOT NULL,     
-    pass_user VARCHAR(150) NOT NULL,          
+    -- Datos usuario
+    nombre_user VARCHAR(150) NOT NULL,
+    pass_user VARCHAR(150) NOT NULL,
     
     -- Control de sistema
     type TINYINT NULL,                         -- Int(1) adaptado a TinyInt
     active BIT DEFAULT 1,                      -- 1 = Activo, 0 = Inactivo.
     clave_usuario VARCHAR(50) NOT NULL,        -- Tomar la primeras iniciales de su nombre y el apellido
     
-    -- Auditoría
+    -- Auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
     Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE), -- Guarda fecha y hora juntas automáticamente
-    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0), -- Nota: Podría ser redundante si usas time
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)), -- Nota: Podría ser redundante si usas time
 
     -- Definición de la Llave Primaria
     CONSTRAINT PK_Usuarios PRIMARY KEY (id_usuario)
@@ -46,9 +42,10 @@ CREATE TABLE Empleado (
     tipo TINYINT NULL,
     active BIT,
     
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
     Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
-    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
     
     -- Definición de la Llave Primaria
     CONSTRAINT PK_Empleado PRIMARY KEY (id_empleado)
@@ -59,9 +56,10 @@ CREATE TABLE DetalleAlerta (
     id_empleado INT NOT NULL,                  -- FK hacia Empleado
     mensaje_alerta VARCHAR(MAX), 
     
-    clave_usuario_u VARCHAR(50) NOT NULL,              -- 'text' se convierte a VARCHAR(MAX)
-    Create_time DATETIME,
-    Create_date DATETIME,
+    -- auditoria
+    clave_usuario_u VARCHAR(50) NOT NULL,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
 
     -- Definición de la Llave Primaria
     CONSTRAINT PK_DetalleAlerta PRIMARY KEY (id_alerta)
@@ -71,9 +69,11 @@ CREATE TABLE ImgPerfil (
     id_img INT IDENTITY(1,1) NOT NULL,      -- Int(10) AUTOINCREMENT
     url_img VARCHAR(MAX) NOT NULL,           
     id_empleado_r INT NOT NULL,                -- FK
+
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME,
-    Create_date DATETIME,    
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)), 
     
     -- Definición de la Llave Primaria
     CONSTRAINT PK_ImgPerfil PRIMARY KEY (id_img)
@@ -82,6 +82,7 @@ CREATE TABLE ImgPerfil (
 CREATE TABLE registro_asistencia (
     id_asistencia INT IDENTITY(1,1) NOT NULL, -- PK Auto-incrementable
     id_empleado INT NOT NULL,                    -- FK 
+    id_empresa INT NOT NULL,
     id_sucursal INT,                             -- FK
     
     fecha DATE,
@@ -91,9 +92,11 @@ CREATE TABLE registro_asistencia (
     retardo BIT,                                 -- Bit(1)
     falta BIT,                                   -- Bit(1)
     
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME,
-    Create_date DATETIME,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
+
     -- Definición de la Llave Primaria
     CONSTRAINT PK_registro_asistencia PRIMARY KEY (id_asistencia)
     
@@ -103,12 +106,14 @@ CREATE TABLE TipoEmpleado (
     id_rol INT IDENTITY(1,1) NOT NULL,        -- PK 
     id_empleado INT NOT NULL,                    -- FK
     nombre_rol VARCHAR(50) NOT NULL,
-    descripcion VARCHAR(50) NOT NULL,
+    descripcion_Templeado VARCHAR(50) NOT NULL,
     id_modulo INT NOT NULL,                      -- FK
     
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME,
-    Create_date DATETIME,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
+
     -- Definición de la Llave Primaria
     CONSTRAINT PK_TipoEmpleado PRIMARY KEY (id_rol)
 );
