2bebba8dd5b1ae7044e415081da6b68821ebc079
Germán <ghove7@gmail.com>
2025-12-11 21:57:43 -0600

    Nomina file update

--- Nomina.sql
+++ Nomina.sql
@@ -6,9 +6,11 @@ CREATE TABLE Nomina (
     id_detalle_pago INT NOT NULL,
     id_periodo INT,
     monto_final DECIMAL(10,2),
+
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME,
-    Create_date DATETIME,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
     
     -- Definición de la Llave Primaria
     CONSTRAINT PK_Nomina PRIMARY KEY (id_nomina)
@@ -22,9 +24,10 @@ CREATE TABLE Detalledenomina (
     id_sucursal INT NOT NULL,
     id_periodo INT NOT NULL,         -- FK: Viene de la tabla Periodo
     sub_monto DECIMAL(10,2),
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME,
-    Create_date DATETIME,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
 
     -- Definición de la Llave Primaria
     CONSTRAINT PK_DetalleNomina PRIMARY KEY (id_detallenomina)
@@ -32,10 +35,12 @@ CREATE TABLE Detalledenomina (
 
 CREATE TABLE Periodo (
     id_periodo INT IDENTITY(1,1) NOT NULL,
-    descripcion VARCHAR(50), -- Para 'semanal', 'quincenal', etc.
+    descripcion_periodo VARCHAR(50), -- Para 'semanal', 'quincenal', etc.
+
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME,
-    Create_date DATETIME,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
 
     -- Definición de la Llave Primaria
     CONSTRAINT PK_Periodo PRIMARY KEY (id_periodo)
@@ -49,9 +54,11 @@ CREATE TABLE catalogo_pagos (
     descripcion VARCHAR(150) NOT NULL,
     monto_base DECIMAL(10,2),
     activo BIT, -- 1 para activo, 0 para inactivo
+
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME,
-    Create_date DATETIME,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
 
     CONSTRAINT PK_CatalogoPagos PRIMARY KEY (id_tipo_pago)
 );
@@ -63,9 +70,11 @@ CREATE TABLE cuentasBancarias (
     id_sucursal INT NOT NULL,
     clave_bancaria VARCHAR(20) NOT NULL, 
     nom_banco VARCHAR(150) NOT NULL,
+
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    create_time DATETIME,
-    create_date DATETIME,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
 
     -- Definición de la Llave Primaria
     CONSTRAINT PK_CuentasBancarias PRIMARY KEY (id_bancaria)
