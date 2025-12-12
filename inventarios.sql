733b776ab4b5cd918409458f94db00f60876ce90
Germán <ghove7@gmail.com>
2025-12-12 01:26:19 -0600

    adding category table to inventary

--- inventarios.sql
+++ inventarios.sql
@@ -12,14 +12,28 @@ CREATE TABLE Inventario (
     -- Inventario
     stock_min INT NOT NULL,
     stock_actual INT NOT NULL DEFAULT 0, -- Se inicializa en 0 si no indicas cantidad
-    
-    -- Auditoría
+
+    -- auditoria
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME NULL,
-    Create_date DATETIME NULL,
+    Create_user_date DATE DEFAULT CAST (GETDATE() AS DATE),
+    Create_user_time TIME(0) DEFAULT CAST (GETDATE() AS TIME(0)),
 
     -- Restricciones (Constraints)
     CONSTRAINT PK_Inventario PRIMARY KEY (id_inventario),
     CONSTRAINT UQ_CodigoBarras UNIQUE (codigo_barras) -- Evita códigos de barras duplicados
 );
 
+CREATE TABLE Categorias (
+    id_categoria INT IDENTITY(1,1) NOT NULL,
+    -- Datos
+    nombre_categoria VARCHAR(50) NOT NULL,
+
+    -- Auditoría
+    clave_usuario_u VARCHAR(50) NOT NULL, -- Quién la creó
+    Create_date DATE DEFAULT CAST(GETDATE() AS DATE),
+    Create_time TIME(0) DEFAULT CAST(GETDATE() AS TIME(0)),
+
+    -- CONSTRAINTS
+    CONSTRAINT PK_Categorias PRIMARY KEY (id_categoria),
+    CONSTRAINT UQ_Nombre_Categoria UNIQUE (nombre_categoria) -- Para no tener dos categorías iguales
+);
