61e2f9995be394a87b04da790c3a51490b0ee619
Germán <ghove7@gmail.com>
2025-12-12 13:22:10 -0600

    Compras_productos file update

--- compra_productos.sql
+++ compra_productos.sql
@@ -1,10 +1,19 @@
-CREATE TABLE compra_productos (
-    id_cp_solicitud INT IDENTITY(1,1) NOT NULL,
+CREATE TABLE Compras (
+    id_compra INT IDENTITY(1,1) NOT NULL,
     id_empresa INT NOT NULL,
     id_sucursal INT NOT NULL,
+
+    folio_factura_proveedor VARCHAR(50),
+    fecha_compra DATETIME DEFAULT GETDATE(),
+    fecha_recepcion DATETIME,
+
     cantidad_productos_total INT NOT NULL, 
     IVA_total DECIMAL(10,2) NOT NULL,
     costo_total DECIMAL(10,2) NOT NULL,
+
+     -- ESTATUS
+    -- 'Pendiente', 'Recibido', 'Pagado'
+    estatus VARCHAR(20) DEFAULT 'Pendiente',
 
     -- Auditoría
     clave_usuario_u VARCHAR(50) NOT NULL,
@@ -12,14 +21,15 @@ CREATE TABLE compra_productos (
     Create_time TIME(0) DEFAULT CAST(GETDATE() AS TIME(0)),
 
     -- Llave Primaria
-    CONSTRAINT PK_compra_productos PRIMARY KEY (id_cp_solicitud)
+    CONSTRAINT PK_compra_productos PRIMARY KEY (id_compra)
 );
 
 CREATE TABLE DetalleCompra (
-    id_dc INT IDENTITY(1,1) NOT NULL,
-    id_cp_solicitud INT NOT NULL, 
+    id_detalle_compra INT IDENTITY(1,1) NOT NULL,
+    id_compra INT NOT NULL, 
     id_proveedor INT NOT NULL,
     id_producto INT NOT NULL,
+
     id_empresa INT NULL,  
     id_sucursal INT NULL, 
     -- Cantidades y Costos
@@ -34,5 +44,5 @@ CREATE TABLE DetalleCompra (
 
     
     -- Llave Primaria
-    CONSTRAINT PK_DetalleCompra PRIMARY KEY (id_dc),
+    CONSTRAINT PK_DetalleCompra PRIMARY KEY (id_detalle_compra),
 );
