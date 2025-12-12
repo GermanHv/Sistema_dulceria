c55597d15e9c93a1bb64b100a808f774a7d3c229
Germán <ghove7@gmail.com>
2025-12-12 01:33:08 -0600

    Empresas file update

--- empresas.sql
+++ empresas.sql
@@ -9,9 +9,11 @@ CREATE TABLE Empresas (
     logo_url VARCHAR(MAX) NOT NULL, -- 'Text' en SQL Server moderno es VARCHAR(MAX)
     website VARCHAR(100) NOT NULL,
     telefono_contacto VARCHAR(10) NOT NULL,
-    id_empleado_r INT NOT NULL, -- FK hacia tabla de Empleados (quien registró la empresa)
-    Create_time DATETIME DEFAULT GETDATE(),
-    Create_date DATETIME DEFAULT GETDATE(),
+   
+   -- Auditoría
+    clave_usuario_u VARCHAR(50) NOT NULL, -- Quién la creó
+    Create_date DATE DEFAULT CAST(GETDATE() AS DATE),
+    Create_time TIME(0) DEFAULT CAST(GETDATE() AS TIME(0)),
 
     CONSTRAINT PK_Empresas PRIMARY KEY (id_empresa)
 );
@@ -19,9 +21,11 @@ CREATE TABLE Empresas (
 CREATE TABLE Sucursal (
     id_sucursal INT IDENTITY(1,1) NOT NULL,
     id_empresa INT NOT NULL, -- FK hacia la tabla Empresas
+
+    -- Auditoría
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME DEFAULT GETDATE(),
-    Create_date DATETIME DEFAULT GETDATE(),
+    Create_date DATE DEFAULT CAST(GETDATE() AS DATE),
+    Create_time TIME(0) DEFAULT CAST(GETDATE() AS TIME(0)),
 
     -- Definición de la Llave Primaria
     CONSTRAINT PK_Sucursal PRIMARY KEY (id_sucursal)
@@ -32,9 +36,12 @@ CREATE TABLE DetalleSucursales (
     nombre_sucursal VARCHAR(50) NOT NULL,
     direccion VARCHAR(255) NOT NULL,
     telefono VARCHAR(20) NULL,
+
+    -- Auditoría
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME NULL,
-    Create_date DATETIME NULL,
+    Create_date DATE DEFAULT CAST(GETDATE() AS DATE),
+    Create_time TIME(0) DEFAULT CAST(GETDATE() AS TIME(0)),
+
     
     -- Definición de la llave primaria
     CONSTRAINT PK_DetalleSucursales PRIMARY KEY (id_sucursal)
@@ -55,9 +62,12 @@ CREATE TABLE cierre_mensual_sucursales (
     
     posicion_ranking INT NOT NULL,
     fecha_procesado DATETIME NULL,
+
+    -- Auditoría
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME NULL,
-    Create_date DATETIME NULL,
+    Create_date DATE DEFAULT CAST(GETDATE() AS DATE),
+    Create_time TIME(0) DEFAULT CAST(GETDATE() AS TIME(0)),
+
 
     -- Definición de llave primaria
     CONSTRAINT PK_cierre_mensual_sucursales PRIMARY KEY (id_cierre_m_s)
@@ -68,9 +78,10 @@ CREATE TABLE InventarioSucursal (
     id_sucursal INT NOT NULL,
     id_empresa INT NOT NULL,
     
+    -- Auditoría
     clave_usuario_u VARCHAR(50) NOT NULL,
-    Create_time DATETIME NULL,
-    Create_date DATETIME NULL,
+    Create_date DATE DEFAULT CAST(GETDATE() AS DATE),
+    Create_time TIME(0) DEFAULT CAST(GETDATE() AS TIME(0)),
     
     -- Definición de la llave primaria
     CONSTRAINT PK_InventarioSucursal PRIMARY KEY (id_inventario)
